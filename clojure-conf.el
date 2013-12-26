99b94021889d28f049022a969bba072d222c4739
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2013-12-27 00:44:35 +0400

    add more beauty to clojurescript mode

--- clojure-conf.el
+++ clojure-conf.el
@@ -1,25 +1,26 @@
-;; bits of internel beauty
+;; bits of internal beauty
 
-(eval-after-load 'clojure-mode
-  '(font-lock-add-keywords
-    'clojure-mode `(("(\\(fn\\)[\[[:space:]]"
-                     (0 (progn (compose-region (match-beginning 1)
-                                               (match-end 1) "λ")
-                               nil))))))
+(dolist (mode '(clojure-mode clojurescript-mode))
+  (eval-after-load mode
+    '(font-lock-add-keywords
+      mode `(("(\\(fn\\)[\[[:space:]]"
+              (0 (progn (compose-region (match-beginning 1)
+                                        (match-end 1) "λ")
+                        nil))))))
 
-(eval-after-load 'clojure-mode
-  '(font-lock-add-keywords
-    'clojure-mode `(("\\(#\\)("
-                     (0 (progn (compose-region (match-beginning 1)
-                                               (match-end 1) "ƒ")
-                               nil))))))
-
-(eval-after-load 'clojure-mode
-  '(font-lock-add-keywords
-    'clojure-mode `(("\\(#\\){"
-                     (0 (progn (compose-region (match-beginning 1)
-                                               (match-end 1) "∈")
-                               nil))))))
+  (eval-after-load mode
+    '(font-lock-add-keywords
+      mode `(("\\(#\\)("
+              (0 (progn (compose-region (match-beginning 1)
+                                        (match-end 1) "ƒ")
+                        nil))))))
+  
+  (eval-after-load mode
+    '(font-lock-add-keywords
+      mode `(("\\(#\\){"
+              (0 (progn (compose-region (match-beginning 1)
+                                        (match-end 1) "∈")
+                        nil)))))))
 
 (eval-after-load 'find-file-in-project
   '(add-to-list 'ffip-patterns "*.clj"))
