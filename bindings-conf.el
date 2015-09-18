5317e5080dcfd5534aff5b547b517c8da11c3c70
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2015-09-18 23:43:10 +0300

    make default font smaller

--- bindings-conf.el
+++ bindings-conf.el
@@ -6,7 +6,7 @@
 
 (setq text-scale-mode-step 1.04)
 ;; List: `Sub-Zoom Font Heights per text-scale-mode-step`
-(defvar sub-zoom-ht (list 130 120 120 110 100 100 90 80 80 80 80 70 70 60 60 50 50 50 40 40 40 30 20 20 20 20 20 20 10 10 10 10 10 10 10 10 10 10 5 5 5 5 5 2 2 2 2 2 2 2 2 1 1 1 1 1 1 1 1 1 1 1 1))
+(defvar sub-zoom-ht (list 120 120 110 100 100 90 80 80 80 80 70 70 60 60 50 50 50 40 40 40 30 20 20 20 20 20 20 10 10 10 10 10 10 10 10 10 10 5 5 5 5 5 2 2 2 2 2 2 2 2 1 1 1 1 1 1 1 1 1 1 1 1))
 (defvar sub-zoom-len (safe-length sub-zoom-ht))
 (defvar def-zoom-ht (car sub-zoom-ht))
 (set-face-attribute 'default nil :height def-zoom-ht)
